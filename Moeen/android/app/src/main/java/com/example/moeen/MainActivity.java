c1e12753b9a638639f25b676863649e2570cc38b
Nuran <161596956+niiinn@users.noreply.github.com>
2025-10-29 20:19:00 +0300

    Add files via upload

---
+++ Moeen/android/app/src/main/java/com/example/moeen/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.moeen;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ Moeen/android/app/src/main/kotlin/com/example/moeen4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.moeen4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
